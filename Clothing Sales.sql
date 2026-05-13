6853a71691977230bca8eff17f569a94635842c4
Aaron Lesserson <57720571+alesserson94@users.noreply.github.com>
2026-05-13 11:12:13 -0400

    Add files via upload

---
+++ Clothing Sales.sql
@@ -0,0 +1,16 @@
+select distinct
+right(t.transaction_id,5) as transaction_id,
+trim(t.product_id) as product_id,sale_date,
+p.product_name,p.brand,t.store_id,s.region,
+abs(quantity) as quantity,
+replace(discount,"N/A",0) as discount,
+round((abs(quantity)*unit_price)*(1-coalesce(discount,0)),2) as total,
+upper(payment_method) as payment_method,sales_rep,is_active
+from transactions t
+left join products p 
+on p.product_id=t.product_id
+left join stores s 
+on s.store_id=t.store_id
+order by transaction_id;
+
+
